8a51b1b30e0d2e6e6de8943a3d4cd63d49ffada4
Anri20 <88647934+Anri20@users.noreply.github.com>
2025-03-30 14:52:06 +0700

    Display all posts

--- index.js
+++ index.js
@@ -28,4 +28,52 @@ const posts = [
     }
 ]
 
-console.log(posts)
+let template = `
+<div class="container main">
+    <div class="post-header">
+        <img class="posters avatar" src="images/avatar-vangogh.jpg" alt="content posters avatar">
+        <div class="posters-data">
+            <label for="post-avatar">Name</label>
+            <address>Location</address>
+        </div>
+    </div>
+    <img class="post" src="images/post-vangogh.jpg" alt="content">
+    <div class="btn-container">
+        <img class="like-button btn" src="images/icon-heart.png" alt="like button">
+        <img class="comment-button btn" src="images/icon-comment.png" alt="comment button">
+        <img class="dm-button btn" src="images/icon-dm.png" alt="dm button">
+    </div>
+    <span class="like-count">100 likes</span>
+    <p><span class="username">username</span> description</p>
+</div>
+`
+
+const main = document.querySelector("main")
+
+// main.innerHTML += template
+
+let stringHTML = ""
+posts.map((post, index) => {
+    console.log(post)
+    stringHTML += `
+    <div class="container main">
+        <div class="post-header">
+            <img class="posters avatar" src=${post.avatar} alt="content posters avatar">
+            <div class="posters-data">
+                <label for="post-avatar">${post.name}</label>
+                <address>${post.location}</address>
+            </div>
+        </div>
+        <img class="post" src=${post.post} alt="content">
+        <div class="btn-container">
+            <img class="like-button btn" src="images/icon-heart.png" alt="like button">
+            <img class="comment-button btn" src="images/icon-comment.png" alt="comment button">
+            <img class="dm-button btn" src="images/icon-dm.png" alt="dm button">
+        </div>
+        <span class="like-count">${post.likes} ${post.likes > 1 ? "likes" : "like"}</span>
+        <p><span class="username">${post.username}</span> ${post.comment}</p>
+    </div>
+    `
+})
+
+main.innerHTML = stringHTML
